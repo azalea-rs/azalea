01a059c20cf3cf9330404d9e408a586500757fe6
mat <github@matdoes.dev>
2021-12-10 21:59:21 +0000

    remove splitter

--- minecraft-chat/src/lib.rs
+++ minecraft-chat/src/lib.rs
@@ -6,7 +6,6 @@ extern crate lazy_static;
 
 pub mod base_component;
 pub mod component;
-pub mod splitter;
 pub mod style;
 pub mod text_component;
 pub mod translatable_component;
--- minecraft-chat/src/splitter.rs
+++
@@ -1,7 +0,0 @@
-//! Used for splitting text
-//! This includes wrapping long lines and applying legacy color formats.
-//! I'm not sure why applying legacy color formats is done here but that's the way Minecraft does it.
-
-use crate::component::Component;
-
-fn split_lines(component: Component) {}
