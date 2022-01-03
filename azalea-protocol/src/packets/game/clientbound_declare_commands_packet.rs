bd87cbb4434ba8bdf16ad93c5353ccefc0497d13
mat <github@matdoes.dev>
2022-01-03 13:36:02 -0600

    create all empty brigadier modules

---
+++ azalea-protocol/src/packets/game/clientbound_declare_commands_packet.rs
@@ -0,0 +1,37 @@
+// use std::hash::Hash;
+
+// use crate::mc_buf::Readable;
+
+// use super::LoginPacket;
+
+// #[derive(Hash, Clone, Debug)]
+// pub struct ClientboundDeclareCommandsPacket {
+//     pub root: RootCommandNode<SharedSuggestionProvider>,
+//     pub public_key: Vec<u8>,
+//     pub nonce: Vec<u8>,
+// }
+
+// impl ClientboundHelloPacket {
+//     pub fn get(self) -> LoginPacket {
+//         LoginPacket::ClientboundHelloPacket(self)
+//     }
+
+//     pub fn write(&self, _buf: &mut Vec<u8>) -> Result<(), std::io::Error> {
+//         panic!("ClientboundHelloPacket::write not implemented")
+//     }
+
+//     pub async fn read<T: tokio::io::AsyncRead + std::marker::Unpin + std::marker::Send>(
+//         buf: &mut T,
+//     ) -> Result<LoginPacket, String> {
+//         let server_id = buf.read_utf_with_len(20).await?;
+//         let public_key = buf.read_byte_array().await?;
+//         let nonce = buf.read_byte_array().await?;
+
+//         Ok(ClientboundHelloPacket {
+//             server_id,
+//             public_key,
+//             nonce,
+//         }
+//         .get())
+//     }
+// }
