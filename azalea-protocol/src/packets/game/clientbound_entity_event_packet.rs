405a00c0d1908a4b3fbd8e6684c77dfb178ac55d
mat <github@matdoes.dev>
2022-06-19 13:54:43 -0500

    Fix ClientboundLevelParticlesPacket
    The particle id is a varint

--- azalea-protocol/src/packets/game/clientbound_entity_event_packet.rs
+++ azalea-protocol/src/packets/game/clientbound_entity_event_packet.rs
@@ -3,6 +3,6 @@ use packet_macros::{GamePacket, McBuf};
 // we can't identify the status in azalea-protocol since they vary depending on the entity
 #[derive(Clone, Debug, McBuf, GamePacket)]
 pub struct ClientboundEntityEventPacket {
-    pub entity_id: i32,
-    pub entity_status: i8,
+    pub entity_id: u32,
+    pub event_id: u8,
 }
