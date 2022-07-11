ec0b6ec06c1607d7bfc9df2fca220f59c700e07b
mat <github@matdoes.dev>
2022-07-10 23:43:09 -0500

    1.19.1-pre4

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A collection of Rust crates primarily for creating Minecraft bots.
 </p>
 
 <!-- The line below is automatically read and updated by the migrate script, so don't change it manually. -->
-*Currently supported Minecraft version: `1.19.1-pre3`.*
+*Currently supported Minecraft version: `1.19.1-pre4`.*
 
 I named this Azalea because it sounds like a cool word and this is a cool library. This project was heavily inspired by PrismarineJS.
 
--- azalea-protocol/src/packets/game/clientbound_player_chat_packet.rs
+++ azalea-protocol/src/packets/game/clientbound_player_chat_packet.rs
@@ -1,22 +1,50 @@
 use azalea_buf::McBuf;
 use azalea_chat::component::Component;
-use azalea_crypto::SaltSignaturePair;
+use azalea_crypto::{MessageSignature, SignedMessageHeader};
 use packet_macros::GamePacket;
+use uuid::Uuid;
 
 #[derive(Clone, Debug, McBuf, GamePacket)]
 pub struct ClientboundPlayerChatPacket {
-    pub signed_content: Component,
-    pub unsigned_content: Option<Component>,
-    #[var]
-    pub type_id: i32,
-    pub sender: ChatSender,
-    pub timestamp: u64,
-    pub salt_signature: SaltSignaturePair,
+    pub message: PlayerChatMessage,
+    pub chat_type: ChatTypeBound,
+}
+
+#[derive(Copy, Clone, Debug, McBuf)]
+pub enum ChatType {
+    Chat = 0,
+    SayCommand = 1,
+    MsgCommandIncoming = 2,
+    MsgCommandOutgoing = 3,
+    TeamMsgCommandIncoming = 4,
+    EmoteCommand = 5,
 }
 
 #[derive(Clone, Debug, McBuf)]
-pub struct ChatSender {
-    pub uuid: uuid::Uuid,
+pub struct ChatTypeBound {
+    pub chat_type: ChatType,
     pub name: Component,
-    pub team_name: Option<Component>,
+    pub target_name: Component,
+}
+
+#[derive(Clone, Debug, McBuf)]
+pub struct PlayerChatMessage {
+    pub signed_header: SignedMessageHeader,
+    pub header_signature: MessageSignature,
+    pub signed_body: SignedMessageBody,
+    pub unsigned_content: Option<Component>,
+}
+
+#[derive(Clone, Debug, McBuf)]
+pub struct SignedMessageBody {
+    pub content: Component,
+    pub timestamp: u64,
+    pub salt: u64,
+    pub last_seen: Vec<LastSeen>,
+}
+
+#[derive(Clone, Debug, McBuf)]
+pub struct LastSeen {
+    pub profile_id: Uuid,
+    pub last_signature: MessageSignature,
 }
