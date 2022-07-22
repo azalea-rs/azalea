3aa856b41325c3c184903aba8fb835557124131e
mat <github@matdoes.dev>
2022-07-21 21:10:20 -0500

    fix wrong imports

--- azalea-protocol/src/packets/game/clientbound_server_data_packet.rs
+++ azalea-protocol/src/packets/game/clientbound_server_data_packet.rs
@@ -1,4 +1,5 @@
 use azalea_buf::McBuf;
+use azalea_chat::component::Component;
 use packet_macros::GamePacket;
 
 #[derive(Clone, Debug, McBuf, GamePacket)]
--- azalea-protocol/src/packets/game/serverbound_chat_ack_packet.rs
+++ azalea-protocol/src/packets/game/serverbound_chat_ack_packet.rs
@@ -1,8 +1,8 @@
-use crate::packets::game::clientbound_player_chat_packet::LastSeenUpdate;
+use crate::packets::game::clientbound_player_chat_packet::LastSeenMessagesUpdate;
 use azalea_buf::McBuf;
 use packet_macros::GamePacket;
 
 #[derive(Clone, Debug, McBuf, GamePacket)]
 pub struct ServerboundChatAckPacket {
-    pub last_seen_messages: LastSeenUpdate,
+    pub last_seen_messages: LastSeenMessagesUpdate,
 }
