7c96916c61b3a0a3d9725cee16fddfbcd81edd08
Ubuntu <github@matdoes.dev>
2022-08-19 19:37:31 +0000

    finish boss event packet

--- azalea-protocol/src/packets/game/clientbound_boss_event_packet.rs
+++ azalea-protocol/src/packets/game/clientbound_boss_event_packet.rs
@@ -1,31 +1,90 @@
-use azalea_buf::McBuf;
+use std::io::{Read, Write};
+
+use azalea_buf::{
+    BufReadError, McBuf, McBufReadable, McBufVarReadable, McBufVarWritable, McBufWritable,
+};
+use azalea_chat::component::Component;
 use packet_macros::ClientboundGamePacket;
 use uuid::Uuid;
 
 #[derive(Clone, Debug, McBuf, ClientboundGamePacket)]
 pub struct ClientboundBossEventPacket {
     pub id: Uuid,
-    pub operation: OperationType, // TODO: Does ClientboundBossEventPacket$Operation::getType, may not be implemented
-    // TODO: {'args': 'packetbuffer', 'field': 'e', 'method': 'a(Lqx;)V', 'name': 'a', 'operation': 'interfacecall', 'target': 'tq$c', 'type': 'interface'}
+    pub operation: Operation,
 }
 
-#[derive(McBuf, Clone, Copy, Debug)]
-pub enum OperationType {
-    Add {
-        pub name: Component,
-        pub progress: f32,
-        pub color: BossBarColor,
-        pub overlay: BossBarOverlay,
-        TODO: these are bitflags
-        pub darken_screen: bool,
-        pub play_music: bool,
-        pub create_world_fog: bool,
-    },
-    Remove = 1,
-    UpdateProgress = 2,
-    UpdateName = 3,
-    UpdateStyle = 4,
-    UpdateProperties = 5,
+#[derive(Clone, Debug)]
+pub enum Operation {
+    Add(AddOperation),
+    Remove,
+    UpdateProgress(f32),
+    UpdateName(Component),
+    UpdateStyle(Style),
+    UpdateProperties(Properties),
+}
+
+impl McBufReadable for Operation {
+    fn read_from(buf: &mut impl Read) -> Result<Self, BufReadError> {
+        let operation_id = u32::var_read_from(buf)?;
+        Ok(match operation_id {
+            0 => Operation::Add(AddOperation::read_from(buf)?),
+            1 => Operation::Remove,
+            2 => Operation::UpdateProgress(f32::read_from(buf)?),
+            3 => Operation::UpdateName(Component::read_from(buf)?),
+            4 => Operation::UpdateStyle(Style::read_from(buf)?),
+            5 => Operation::UpdateProperties(Properties::read_from(buf)?),
+            _ => {
+                return Err(BufReadError::UnexpectedEnumVariant {
+                    id: operation_id as i32,
+                })
+            }
+        })
+    }
+}
+
+impl McBufWritable for Operation {
+    fn write_into(&self, buf: &mut impl Write) -> Result<(), std::io::Error> {
+        match self {
+            Operation::Add(add) => {
+                0u32.var_write_into(buf)?;
+                add.write_into(buf)?;
+            }
+            Operation::Remove => {
+                1u32.var_write_into(buf)?;
+            }
+            Operation::UpdateProgress(progress) => {
+                2u32.var_write_into(buf)?;
+                progress.write_into(buf)?;
+            }
+            Operation::UpdateName(name) => {
+                3u32.var_write_into(buf)?;
+                name.write_into(buf)?;
+            }
+            Operation::UpdateStyle(style) => {
+                4u32.var_write_into(buf)?;
+                style.write_into(buf)?;
+            }
+            Operation::UpdateProperties(properties) => {
+                5u32.var_write_into(buf)?;
+                properties.write_into(buf)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, McBuf)]
+pub struct AddOperation {
+    name: Component,
+    progress: f32,
+    style: Style,
+    properties: Properties,
+}
+
+#[derive(Clone, Debug, McBuf)]
+pub struct Style {
+    color: BossBarColor,
+    overlay: BossBarOverlay,
 }
 
 #[derive(McBuf, Clone, Copy, Debug)]
@@ -46,4 +105,39 @@ pub enum BossBarOverlay {
     Notched10 = 2,
     Notched12 = 3,
     Notched20 = 4,
+}
+
+#[derive(Clone, Debug)]
+pub struct Properties {
+    pub darken_screen: bool,
+    pub play_music: bool,
+    pub create_world_fog: bool,
+}
+
+impl McBufReadable for Properties {
+    fn read_from(buf: &mut impl Read) -> Result<Self, BufReadError> {
+        let byte = u8::read_from(buf)?;
+        Ok(Self {
+            darken_screen: byte & 1 != 0,
+            play_music: byte & 2 != 0,
+            create_world_fog: byte & 4 != 0,
+        })
+    }
+}
+
+impl McBufWritable for Properties {
+    fn write_into(&self, buf: &mut impl Write) -> Result<(), std::io::Error> {
+        let mut byte = 0;
+        if self.darken_screen {
+            byte |= 1;
+        }
+        if self.play_music {
+            byte |= 2;
+        }
+        if self.create_world_fog {
+            byte |= 4;
+        }
+        u8::write_into(&byte, buf)?;
+        Ok(())
+    }
 }
