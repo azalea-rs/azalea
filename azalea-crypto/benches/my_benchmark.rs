250432a3f42e6c19c20cf29e5953409d38af8a31
mat <github@matdoes.dev>
2022-08-28 15:10:50 -0500

    decrypting benchmark

--- azalea-crypto/benches/my_benchmark.rs
+++ azalea-crypto/benches/my_benchmark.rs
@@ -1,16 +1,22 @@
-use azalea_crypto::{create_cipher, encrypt_packet};
+use azalea_crypto::{create_cipher, decrypt_packet, encrypt_packet};
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 
 fn bench(c: &mut Criterion) {
-    let (enc, dec) = create_cipher(b"0123456789abcdef");
+    let (mut enc, mut dec) = create_cipher(b"0123456789abcdef");
 
     let mut packet = [0u8; 65536];
     for i in 0..packet.len() {
         packet[i] = i as u8;
     }
 
-    c.bench_function("Encrypt", |b| {
+    c.bench_function("Encrypt 64kb", |b| {
         b.iter(|| encrypt_packet(&mut enc.clone(), &mut packet.clone()))
+    });
+
+    encrypt_packet(&mut enc, &mut packet);
+
+    c.bench_function("Decrypt 64kb", |b| {
+        b.iter(|| decrypt_packet(&mut dec.clone(), &mut packet.clone()))
     });
 }
 
