58f8e95fe864332cecbc9d4fb592e7bdb3dd975f
mat <github@matdoes.dev>
2022-09-11 13:04:26 -0500

    no unused variable

--- azalea-protocol/azalea-protocol-macros/src/lib.rs
+++ azalea-protocol/azalea-protocol-macros/src/lib.rs
@@ -247,11 +247,13 @@ pub fn declare_state_packets(input: TokenStream) -> TokenStream {
         clientbound_read_match_contents.extend(quote! {
             #id => {
                 let data = #module::#name::read(buf).map_err(|e| crate::read::ReadPacketError::Parse { source: e, packet_id: #id, packet_name: #name_litstr.to_string() })?;
-                let mut leftover = Vec::new();
-                let _ = buf.read_to_end(&mut leftover);
                 #[cfg(debug_assertions)]
-                if !leftover.is_empty() {
-                    return Err(crate::read::ReadPacketError::LeftoverData { packet_name: #name_litstr.to_string(), data: leftover });
+                {
+                    let mut leftover = Vec::new();
+                    let _ = buf.read_to_end(&mut leftover);
+                    if !leftover.is_empty() {
+                        return Err(crate::read::ReadPacketError::LeftoverData { packet_name: #name_litstr.to_string(), data: leftover });
+                    }
                 }
                 data
             },
