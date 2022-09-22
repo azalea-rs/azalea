3fd98ae287b9c20584a7c7b9ba89dbb3c44b8473
Ubuntu <github@matdoes.dev>
2022-09-22 00:38:19 +0000

    update potato bot example

--- azalea/examples/potatobot/main.rs
+++ azalea/examples/potatobot/main.rs
@@ -1,15 +1,9 @@
-use azalea::{Account, Client, Event, MoveDirection, pathfinder, Vec3};
-use std::convert::TryInto;
+use azalea::{Account, Client, Event, MoveDirection, pathfinder, Vec3, BlockPos, ItemKind};
+use std::{convert::TryInto, sync::{Arc, MutexGuard, Mutex}};
 
-// Custom state defined for every bot. Use this to make the bot remember
-// things.
 #[derive(Default)]
 struct State {
-    pub farm_task: FarmTask,
     pub eating: bool,
-    // To use a plugin, simply add it like this to your state. You'll also have
-    // to call its event handler every time you get an event.
-    pub pf: pathfinder::Plugin,
 }
 
 #[tokio::main]
@@ -18,86 +12,86 @@ async fn main() {
 
     let account = Account::offline("bot");
     let (bot, mut rx) = account.join(&"localhost".try_into().unwrap()).await.unwrap();
-    let state = Arc::new(State::default());
+    let state = Arc::new(Mutex::new(State::default()));
+    let pathfinder_state = Arc::new(Mutex::new(pathfinder::State::default()));
 
     // Maybe this (along with state stuff) could be turned into a macro in
     // the future?
     while let Some(event) = rx.recv().await {
         // You must do this for every plugin. If you want to disable a plugin,
         // simply don't call its event handler.
-        bot.pf.handle(event, bot);
-        handle(bot, event, &mut state);
+        tokio::spawn(async {
+            autoeat_handle(bot, event, state.clone()).await;
+            pathfinder::handle(bot, event, pathfinder_state.clone()).await;
+            handle(bot.clone(), event, state.clone()).await;
+        });
     }
 }
 
-fn handle(bot: &mut Client, event: Event, state: &mut State) {
+async fn handle(state: State, bot: Client, event: Event) -> anyhow::Result<()> {
     match event {
-        Event::Tick => tick(bot, state),
-        Event::Login => {},
-        Event::Packet(packet) => {},
+        Event::Login => {
+            goto_farm(state, bot).await?;
+            // after we get to the farm, start farming
+
+        },
+        Event::Tick => {
+        },
+        Event::Packet(_) => {},
         _ => {}
     }
+
+    Ok(())
 }
 
-// Figure out what we should do.
-fn tick(bot: &mut Client, state: &mut State) {
-    // If we're currently pathfinding somewhere, don't do anything (except eat maybe)
-    if state.pf.pathing {
-        tick_eat(bot, state);
-        return;
-    };
-    if let FarmTask::Depositing { .. } = state.farm_task {
-        tick_deposit(bot, state);
-        return;
-    }
 
-    // If we're not sure we're at the farm (we just spawned or respawned), go
-    // there.
-    if !state.at_farm {
-        // note that nothing is actually executed until the end of the tick
-        bot.state.goto(pathfinder::Goals::Near(5, azalea::BlockPos::new(0, 70, 0)));
-        // the name "at_farm" is a little misleading since it'll be true if
-        // we're going towards the farm and not actually there, but ehh
-        state.at_farm = true;
-        return;
-    }
-    
-    tick_farm(bot, state);
+// go to the place where we start farming
+async fn goto_farm(state: State, bot: Client, event: Event) -> anyhow::Result<()> {
+    bot.state.goto(
+        pathfinder::Goals::Near(5, BlockPos::new(0, 70, 0))
+    ).await?;
+    Ok(())
 }
 
-/// Eat something if we're hungry.
-fn tick_eat(bot: &mut Client, state: &mut State) {
-    if !bot.using_held_item() && bot.food_level() <= 17 {
-        if let Slot::Present(_) = bot.hold(azalea::ItemGroup::Food) {
-            bot.use_held_item();
+async fn autoeat_handle(state: Arc<Mutex<State>>, bot: &mut , event: Event) {
+    match event {
+        Event::UpdateHunger => {
+            if !bot.using_held_item() && bot.food_level() <= 17 {
+                if let azalea::Slot::Present(_) = bot.hold(azalea::ItemGroup::Food).await {
+                    bot.use_held_item().await;
+                }
+            }
         }
     }
 }
 
-#[derive(Default)]
+
+#[derive(Default, Clone)]
 enum FarmTask {
     #[default]
-    GoingToFarm,
-    Depositing { at_deposit_area: bool }
+    GoToFarm,
+    Farm,
+    Deposit 
 }
 
-fn tick_farm(bot: &mut Client, state: &mut State) {
-    if bot.inventory.is_full() {
-    }
-}
 
 // go to the chest and deposit everything in our inventory.
-fn tick_deposit(bot: &mut Client, state: &mut State) {
-    let FarmTask::Depositing { at_deposit_area } = state.farm_task;
-    
-    if !at_deposit_area {
-        bot.state.goto(Vec3::new ( 0, 70, 0 ));
-        let chest = bot.open_container(&bot.world.block_at(Vec3 { 0, 70, 0 }).await.unwrap();
-    };
+async fn deposit(bot: &mut Client, state: &mut State) -> anyhow::Result<()> {
+    // first throw away any garbage we might have
+    bot.toss(
+        |item| item.kind != ItemKind::Potato && item.kind != ItemKind::DiamondHoe
+    );
 
-    for item in bot.inventory().items() {
-        chest.deposit(item);
+    bot.state.goto(Vec3::new ( 0, 70, 0 )).await?;
+    let chest = bot.open_container(&bot.world.block_at(BlockPos::new(0, 70, 0)).await.unwrap();
+
+    let inventory_potato_count: usize = bot.inventory().count_total(|item| item.kind == ItemKind::Potato);;
+    if inventory_potato_count > 64 {
+        chest.deposit_total_count(
+            |item| item.kind == azalea::ItemKind::Potato,
+            inventory_potato_count - 64
+        ).await;
     }
-    chest.close();
-    return;
+    chest.close().await;
+    Ok(())
 }
--- azalea/src/lib.rs
+++ azalea/src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! This is currently an advertisement crate for
+//! [Azalea](https://github.com/mat-1/azalea). More stuff will be here soon!
