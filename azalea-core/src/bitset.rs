5661a14ad633222a0e761308709c7103b98cbd68
mat <github@matdoes.dev>
2022-09-24 15:23:43 -0500

    implement BitSet::clear

--- azalea-core/src/bitset.rs
+++ azalea-core/src/bitset.rs
@@ -18,6 +18,62 @@ impl BitSet {
     pub fn index(&self, index: usize) -> bool {
         (self.data[index / 64] & (1u64 << (index % 64))) != 0
     }
+
+    fn check_range(&self, from_index: usize, to_index: usize) {
+        assert!(
+            from_index <= to_index,
+            "fromIndex: {} > toIndex: {}",
+            from_index,
+            to_index
+        );
+    }
+
+    fn word_index(&self, bit_index: usize) -> usize {
+        bit_index >> 6
+    }
+
+    pub fn clear(&mut self, from_index: usize, mut to_index: usize) {
+        self.check_range(from_index, to_index);
+
+        if from_index == to_index {
+            return;
+        }
+
+        let start_word_index = self.word_index(from_index);
+        if start_word_index >= self.data.len() {
+            return;
+        }
+
+        let mut end_word_index = self.word_index(to_index - 1);
+        if end_word_index >= self.data.len() {
+            to_index = self.len();
+            end_word_index = self.data.len() - 1;
+        }
+
+        let first_word_mask = u64::MAX << from_index;
+        let last_word_mask = u64::MAX >> (64 - (to_index % 64));
+        if start_word_index == end_word_index {
+            // Case 1: One word
+            self.data[start_word_index] &= !(first_word_mask & last_word_mask);
+        } else {
+            // Case 2: Multiple words
+            // Handle first word
+            self.data[start_word_index] &= !first_word_mask;
+
+            // Handle intermediate words, if any
+            for i in start_word_index + 1..end_word_index {
+                self.data[i] = 0;
+            }
+
+            // Handle last word
+            self.data[end_word_index] &= !last_word_mask;
+        }
+    }
+
+    /// Returns the maximum potential items in the BitSet. This will be divisible by 64.
+    fn len(&self) -> usize {
+        self.data.len() * 64
+    }
 }
 
 impl McBufReadable for BitSet {
@@ -54,5 +110,23 @@ mod tests {
         assert_eq!(bitset.index(0), false);
         assert_eq!(bitset.index(1), true);
         assert_eq!(bitset.index(2), false);
+    }
+
+    #[test]
+    fn test_clear() {
+        let mut bitset = BitSet::new(128);
+        bitset.set(62);
+        bitset.set(63);
+        bitset.set(64);
+        bitset.set(65);
+        bitset.set(66);
+
+        bitset.clear(63, 65);
+
+        assert_eq!(bitset.index(62), true);
+        assert_eq!(bitset.index(63), false);
+        assert_eq!(bitset.index(64), false);
+        assert_eq!(bitset.index(65), true);
+        assert_eq!(bitset.index(66), true);
     }
 }
