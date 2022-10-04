940ad47eb143bb44dc1a09bc1073284a3d113998
Ubuntu <github@matdoes.dev>
2022-10-04 21:08:29 +0000

    more work on d* lite

--- azalea-pathfinder/src/dstarlite.rs
+++ azalea-pathfinder/src/dstarlite.rs
@@ -1,36 +1,54 @@
-//! An implementation of D* Lite (optimized version) from
+//! An implementation of D* Lite (optimized version) as described in
 //! http://idm-lab.org/bib/abstracts/papers/aaai02b.pdf
+//!
+//! Note that the words "vertex" and "key" from the paper are replaced with
+//! "node" and "priority" in this code because I think these terms better
+//! represent their purpose and are more commonly used when describing similar
+//! algorithms.
 
+use priority_queue::PriorityQueue;
 use std::collections::BinaryHeap;
 
-#[derive(Default, Ord)]
-pub struct Vertex<Weight>
+#[derive(Default, Eq, PartialEq, Hash)]
+pub struct Node<W>
 where
-    Weight: Default + Ord,
+    W: Default + Ord,
 {
-    pub g: Weight,
-    pub rhs: Weight,
+    pub g: W,
+    pub rhs: W,
 }
 
-type Heuristic<Weight> = fn(start: &Vertex<Weight>, current: &Vertex<Weight>) -> Weight;
-
-pub struct DStarLite<Weight>
+type Heuristic<W>
 where
-    Weight: Default + Ord,
+    W: Default + Ord,
+= fn(start: &Node<W>, current: &Node<W>) -> W;
+
+pub struct DStarLite<W>
+where
+    W: Default + Ord,
 {
-    pub start: Vertex<Weight>,
-    pub goal: Vertex<Weight>,
+    pub start: Node<W>,
+    pub goal: Node<W>,
 
-    pub queue: BinaryHeap<Vertex<Weight>>,
+    pub queue: PriorityQueue<Node<W>, Priority<W>>,
 
-    pub k_m: Weight,
-    pub heuristic: Heuristic<Weight>,
+    pub k_m: W,
+    pub heuristic: Heuristic<W>,
 }
 
-impl<Weight> DStarLite<Weight> {
-    fn calculate_key(&self, s: &Vertex<Weight>) -> (Weight, Weight) {
+// rust does lexicographic ordering by default when we derive Ord
+#[derive(Eq, Ord, PartialEq, PartialOrd)]
+pub struct Priority<W>(W, W)
+where
+    W: Default + Ord;
+
+impl<W> DStarLite<W>
+where
+    W: Default + Ord,
+{
+    fn calculate_key(&self, s: &Node<W>) -> Priority<W> {
         // return [min(g(s), rhs(s)) + h(s_start, s) + k_m, min(g(s), rhs(s))]
-        (
+        Priority(
             s.g.min(s.rhs) + self.heuristic(self.start, s) + self.k_m,
             s.g.min(s.rhs),
         )
@@ -40,12 +58,16 @@ impl<Weight> DStarLite<Weight> {
         let priority_queue = BinaryHeap::new();
         let k_m = 0;
         self.goal.rhs = 0;
-        priority_queue.push(self.goal, (self.heuristic(&self.start, self.goal), 0));
+        priority_queue.push(
+            self.goal,
+            Priority(self.heuristic(&self.start, self.goal), 0),
+        );
     }
 
-    fn update_vertex(&self, u: Vertex, priority_queue: BinaryHeap<Vertex>) {
+    fn update_vertex(&self, u: Node<W>, priority_queue: PriorityQueue<Node<W>, Priority<W>>) {
         // if(g(u)) != rhs(u) AND u is in U) U.Update(u, calculate_key(u))
-        // if u.g != u.rhs && priority_queue.contains(u) {
-        // 	priority_queue.update(u, calculate_key(s_start, u, h));
+        if u.g != u.rhs && priority_queue.contains(u) {
+            priority_queue.update(u, self.calculate_key(s_start, u, h));
+        }
     }
 }
