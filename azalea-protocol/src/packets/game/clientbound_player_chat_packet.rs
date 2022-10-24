9d02bcb71a92f47baa4ee6308427ee1b9df20987
Ubuntu <github@matdoes.dev>
2022-10-24 17:57:36 +0000

    work a bit more on 22w42a

--- azalea-protocol/src/packets/game/clientbound_player_chat_packet.rs
+++ azalea-protocol/src/packets/game/clientbound_player_chat_packet.rs
@@ -1,5 +1,8 @@
-use azalea_buf::McBuf;
+use std::io::{Cursor, Write};
+
+use azalea_buf::{BufReadError, McBuf, McBufReadable, McBufWritable};
 use azalea_chat::component::Component;
+use azalea_crypto::MessageSignature;
 use azalea_protocol_macros::ClientboundGamePacket;
 use uuid::Uuid;
 
@@ -8,10 +11,7 @@ pub struct ClientboundPlayerChatPacket {
     pub sender: Uuid,
     #[var]
     pub index: u32,
-    pub signature: Option<Vec<u8>>,
-    // TODO: {'field': 'd.a', 'operation': 'write', 'type': 'string'}
-    // TODO: {'field': 'd.b.toEpochMilli()', 'operation': 'write', 'type': 'long'}
-    // TODO: {'field': 'd.c', 'operation': 'write', 'type': 'long'}
+    pub signature: Option<MessageSignature>,
     pub body: todo!(),
     pub unsigned_content: Option<Component>,
     // TODO: {'field': 'f.f', 'operation': 'write', 'type': 'enum'}
@@ -19,4 +19,49 @@ pub struct ClientboundPlayerChatPacket {
     // TODO: {'field': 'g.a', 'operation': 'write', 'type': 'varint'}
     // TODO: {'field': 'g.b', 'operation': 'write', 'type': 'chatcomponent'}
     pub chat_type: Option<Component>,
+}
+
+#[derive(McBuf)]
+pub struct PackedSignedMessageBody {
+    pub content: String,
+    pub timestamp: u64,
+    pub salt: u64,
+    pub last_seen: PackedLastSeenMessages,
+}
+
+pub struct PackedLastSeenMessages {
+    pub entries: PackedMessageSignature,
+}
+
+/// Messages can be deleted by either their signature or message id.
+#[derive(Clone, Debug)]
+pub enum PackedMessageSignature {
+    Signature(MessageSignature),
+    Id(u32),
+}
+
+impl McBufReadable for PackedMessageSignature {
+    fn read_from(buf: &mut Cursor<&[u8]>) -> Result<Self, BufReadError> {
+        let id = u32::var_read_from(buf)?;
+        if id == 0 {
+            let full_signature = MessageSignature::read_from(buf)?;
+            Ok(PackedMessageSignature::Signature(full_signature))
+        } else {
+            Ok(PackedMessageSignature::Id(id - 1))
+        }
+    }
+}
+impl McBufWritable for PackedMessageSignature {
+    fn write_into(&self, buf: &mut impl Write) -> Result<(), std::io::Error> {
+        match self {
+            PackedMessageSignature::Signature(full_signature) => {
+                0u32.var_write_into(buf);
+                full_signature.write_into(buf);
+            }
+            PackedMessageSignature::Id(id) => {
+                (id + 1).var_write_into(buf);
+            }
+        }
+        Ok(())
+    }
 }
