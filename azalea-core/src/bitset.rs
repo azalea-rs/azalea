8b3fe0b241c9e3100c799e9fef2d1fa45ec64464
Ubuntu <github@matdoes.dev>
2022-11-10 15:46:11 +0000

    fix another panic in bitset

--- azalea-core/src/bitset.rs
+++ azalea-core/src/bitset.rs
@@ -88,7 +88,7 @@ impl BitSet {
             return from_index;
         }
 
-        let mut word = !self.data[u] & (u64::MAX << from_index);
+        let mut word = !self.data[u] & (u64::MAX.wrapping_shl(from_index));
 
         loop {
             if word != 0 {
