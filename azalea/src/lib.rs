e6db7d5f2ffa88e95965a421b13e54b83aaef175
mat <github@matdoes.dev>
2022-11-11 21:55:47 -0600

    add optimization stuff to azalea docs

--- azalea/src/lib.rs
+++ azalea/src/lib.rs
@@ -11,11 +11,25 @@
 //! default nightly`.
 //!
 //! Then, add one of the following lines to your Cargo.toml:
-//! 
+//!
 //! Latest bleeding-edge version:
 //! `azalea = { git="https://github.com/mat-1/Cargo.toml" }`\
 //! Latest "stable" release:
 //! `azalea = "0.3"`
+//!
+//! # Optimization
+//!
+//! For faster compile times, make a .cargo/config.toml file in your project
+//! and copy
+//! [this file](https://github.com/mat-1/azalea/blob/main/.cargo/config.toml)
+//! into it.
+//!
+//! For faster performance in debug mode, add
+//! ```toml
+//! [profile.dev.package.*]
+//! opt-level = 3
+//! ```
+//! to your Cargo.toml.
 //!
 //! # Examples
 //!
