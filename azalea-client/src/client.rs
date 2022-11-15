709c5dbc19ffb8c47e001b1d5c14846ee69e7988
Ubuntu <github@matdoes.dev>
2022-11-15 20:44:33 +0000

    add Client::metadata

--- azalea-client/src/client.rs
+++ azalea-client/src/client.rs
@@ -910,6 +910,12 @@ impl Client {
 
         Ok(())
     }
+
+    /// Get the player metadata for ourselves. You can use this to get your
+    /// health, xp score, and other useful information.
+    pub fn metadata(&self) -> metadata::Player {
+        self.entity().metadata.clone().into_player().unwrap()
+    }
 }
 
 impl<T> From<std::sync::PoisonError<T>> for HandleError {
