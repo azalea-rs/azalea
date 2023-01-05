90df0fa422620c8061d5082452bef6167491e44c
Shayne Hartford <shaybox@shaybox.com>
2023-01-05 15:28:29 -0500

    Unnecessary reference

--- azalea-language/src/lib.rs
+++ azalea-language/src/lib.rs
@@ -3,10 +3,8 @@
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 
-pub static STORAGE: Lazy<HashMap<String, String>> = Lazy::new(|| {
-    serde_json::from_str(&include_str!("en_us.json"))
-    .unwrap()
-});
+pub static STORAGE: Lazy<HashMap<String, String>> =
+    Lazy::new(|| serde_json::from_str(include_str!("en_us.json")).unwrap());
 
 pub fn get(key: &str) -> Option<&str> {
     STORAGE.get(key).map(|s| s.as_str())
