8078a61f1a6c5e9c25ea317d1e1fad1ce79ecfda
0x45 <jacobbarany@gmail.com>
2023-03-23 23:48:36 +1000

    Update moves.rs

--- azalea/src/pathfinder/moves.rs
+++ azalea/src/pathfinder/moves.rs
@@ -5,20 +5,18 @@ use azalea_world::Instance;
 
 /// whether this block is passable
 fn is_block_passable(pos: &BlockPos, world: &Instance) -> bool {
-    if let Some(block) = world.chunks.get_block_state(pos) {
-        block.shape() == &collision::empty_shape()
-    } else {
-        false
-    }
+    world
+        .chunks
+        .get_block_state(pos)
+        .map_or(false, |block| block.shape() == &collision::empty_shape())
 }
 
 /// whether this block has a solid hitbox (i.e. we can stand on it)
 fn is_block_solid(pos: &BlockPos, world: &Instance) -> bool {
-    if let Some(block) = world.chunks.get_block_state(pos) {
-        block.shape() == &collision::block_shape()
-    } else {
-        false
-    }
+    world
+        .chunks
+        .get_block_state(pos)
+        .map_or(false, |block| block.shape() == &collision::block_shape())
 }
 
 /// Whether this block and the block above are passable
@@ -26,9 +24,8 @@ fn is_passable(pos: &BlockPos, world: &Instance) -> bool {
     is_block_passable(pos, world) && is_block_passable(&pos.up(1), world)
 }
 
-/// Whether we can stand in this position. Checks if the block below is solid,
+/// Whether we can stand in this position. Checks if the block below is solid,	
 /// and that the two blocks above that are passable.
-
 fn is_standable(pos: &BlockPos, world: &Instance) -> bool {
     is_block_solid(&pos.down(1), world) && is_passable(pos, world)
 }
@@ -38,8 +35,6 @@ const WALK_ONE_BLOCK_COST: f32 = 1.0;
 
 pub trait Move: Send + Sync {
     fn cost(&self, world: &Instance, node: &Node) -> f32;
-    /// Returns by how much the entity's position should be changed when this
-    /// move is executed.
     fn offset(&self) -> BlockPos;
     fn next_node(&self, node: &Node) -> Node {
         Node {
@@ -49,84 +44,38 @@ pub trait Move: Send + Sync {
     }
 }
 
-pub struct ForwardMove(pub CardinalDirection);
-impl Move for ForwardMove {
-    fn cost(&self, world: &Instance, node: &Node) -> f32 {
-        if is_standable(&(node.pos + self.offset()), world)
-            && node.vertical_vel == VerticalVel::None
-        {
-            WALK_ONE_BLOCK_COST
-        } else {
-            f32::INFINITY
+macro_rules! impl_move {
+    ($name:ident, $y_offset:expr, $cost:expr) => {
+        pub struct $name(pub CardinalDirection);
+        impl Move for $name {
+            fn cost(&self, world: &Instance, node: &Node) -> f32 {
+                if is_standable(&(node.pos + self.offset()), world)
+                    && node.vertical_vel == VerticalVel::None
+                {
+                    $cost
+                } else {
+                    f32::INFINITY
+                }
+            }
+            fn offset(&self) -> BlockPos {
+                BlockPos::new(self.0.x(), $y_offset, self.0.z())
+            }
         }
-    }
-    fn offset(&self) -> BlockPos {
-        BlockPos::new(self.0.x(), 0, self.0.z())
-    }
+    };
 }
 
-pub struct AscendMove(pub CardinalDirection);
-impl Move for AscendMove {
-    fn cost(&self, world: &Instance, node: &Node) -> f32 {
-        if node.vertical_vel == VerticalVel::None
-            && is_block_passable(&node.pos.up(2), world)
-            && is_standable(&(node.pos + self.offset()), world)
-        {
-            WALK_ONE_BLOCK_COST + JUMP_COST
-        } else {
-            f32::INFINITY
-        }
-    }
-    fn offset(&self) -> BlockPos {
-        BlockPos::new(self.0.x(), 1, self.0.z())
-    }
-    fn next_node(&self, node: &Node) -> Node {
-        Node {
-            pos: node.pos + self.offset(),
-            vertical_vel: VerticalVel::None,
-        }
-    }
-}
-pub struct DescendMove(pub CardinalDirection);
-impl Move for DescendMove {
-    fn cost(&self, world: &Instance, node: &Node) -> f32 {
-        // check whether 3 blocks vertically forward are passable
-        if node.vertical_vel == VerticalVel::None
-            && is_standable(&(node.pos + self.offset()), world)
-            && is_block_passable(&(node.pos + self.offset().up(2)), world)
-        {
-            WALK_ONE_BLOCK_COST
-        } else {
-            f32::INFINITY
-        }
-    }
-    fn offset(&self) -> BlockPos {
-        BlockPos::new(self.0.x(), -1, self.0.z())
-    }
-    fn next_node(&self, node: &Node) -> Node {
-        Node {
-            pos: node.pos + self.offset(),
-            vertical_vel: VerticalVel::None,
-        }
-    }
-}
+impl_move!(ForwardMove, 0, WALK_ONE_BLOCK_COST);
+impl_move!(AscendMove, 1, WALK_ONE_BLOCK_COST + JUMP_COST);
+impl_move!(DescendMove, -1, WALK_ONE_BLOCK_COST);
+
 pub struct DiagonalMove(pub CardinalDirection);
 impl Move for DiagonalMove {
     fn cost(&self, world: &Instance, node: &Node) -> f32 {
         if node.vertical_vel != VerticalVel::None {
             return f32::INFINITY;
         }
-        if !is_passable(
-            &BlockPos::new(node.pos.x + self.0.x(), node.pos.y, node.pos.z + self.0.z()),
-            world,
-        ) && !is_passable(
-            &BlockPos::new(
-                node.pos.x + self.0.right().x(),
-                node.pos.y,
-                node.pos.z + self.0.right().z(),
-            ),
-            world,
-        ) {
+        let diagonal_pos = node.pos + self.offset();
+        if !is_passable(&diagonal_pos, world) && !is_passable(&diagonal_pos.up(1), world) {
             return f32::INFINITY;
         }
         if !is_standable(&(node.pos + self.offset()), world) {
@@ -137,12 +86,6 @@ impl Move for DiagonalMove {
     fn offset(&self) -> BlockPos {
         let right = self.0.right();
         BlockPos::new(self.0.x() + right.x(), 0, self.0.z() + right.z())
-    }
-    fn next_node(&self, node: &Node) -> Node {
-        Node {
-            pos: node.pos + self.offset(),
-            vertical_vel: VerticalVel::None,
-        }
     }
 }
 
