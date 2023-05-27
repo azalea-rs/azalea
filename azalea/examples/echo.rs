2a2169226fb112082d91e8fca918c82cb47db7ef
mat <git@matdoes.dev>
2023-05-27 14:30:56 -0500

    revert echo example

--- azalea/examples/echo.rs
+++ azalea/examples/echo.rs
@@ -4,12 +4,12 @@ use azalea::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    // let account = Account::offline("bot");
-    let account = Account::microsoft("minecraft3@matdoes.dev").await.unwrap();
+    let account = Account::offline("bot");
+    // or let account = Account::microsoft("email").await.unwrap();
 
     ClientBuilder::new()
         .set_handler(handle)
-        .start(account, "85.190.131.233")
+        .start(account, "localhost")
         .await
         .unwrap();
 }
@@ -18,7 +18,6 @@ async fn main() {
 pub struct State {}
 
 async fn handle(bot: Client, event: Event, _state: State) -> anyhow::Result<()> {
-    std::process::exit(0);
     match event {
         Event::Chat(m) => {
             if let (Some(sender), content) = m.split_sender_and_content() {
