350e32d0a9f21103e271e80e63daa5711a78e1d7
EnderKill98 <117233381+EnderKill98@users.noreply.github.com>
2024-06-16 23:03:34 +0200

    Fix command packets (#155)
    * Fix copy&paste mistake from chat command signed packet to chat command packet
    
    * Bugfix
    
    ---------
    
    Co-authored-by: EnderKill98 <no@mail.tld>

--- azalea-protocol/src/packets/game/serverbound_chat_command_packet.rs
+++ azalea-protocol/src/packets/game/serverbound_chat_command_packet.rs
@@ -1,19 +1,7 @@
-use super::serverbound_chat_packet::LastSeenMessagesUpdate;
 use azalea_buf::McBuf;
-use azalea_crypto::MessageSignature;
 use azalea_protocol_macros::ServerboundGamePacket;
 
 #[derive(Clone, Debug, McBuf, ServerboundGamePacket)]
 pub struct ServerboundChatCommandPacket {
     pub command: String,
-    pub timestamp: u64,
-    pub salt: u64,
-    pub argument_signatures: Vec<ArgumentSignature>,
-    pub last_seen_messages: LastSeenMessagesUpdate,
-}
-
-#[derive(Clone, Debug, McBuf)]
-pub struct ArgumentSignature {
-    pub name: String,
-    pub signature: MessageSignature,
 }
