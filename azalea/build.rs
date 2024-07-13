3a1d52dd7444d578675aa21f02aa55c787daa29c
Paddi <secure@cryne.me>
2024-07-13 14:25:55 +0200

    Check env vars first, fall back to rustc in $PATH

--- azalea/build.rs
+++ azalea/build.rs
@@ -1,7 +1,11 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
-    let rustc_version_output = Command::new("rustc").arg("-V").output().unwrap();
+    let rustc_command = env::var("RUSTC")
+        .or_else(|_| env::var("CARGO_BUILD_RUSTC"))
+        .unwrap_or(String::from("rustc"));
+    let rustc_version_output = Command::new(rustc_command).arg("-V").output().unwrap();
     if !rustc_version_output.status.success()
         || !String::from_utf8(rustc_version_output.stdout)
             .unwrap()
