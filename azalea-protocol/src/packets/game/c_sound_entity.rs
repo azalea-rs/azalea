900a4234e5b7fbf24a557907bce7c3b7ce30822e
mat <git@matdoes.dev>
2025-01-21 23:21:08 +0000

    fix wrong ClientboundSoundEntity

--- azalea-protocol/src/packets/game/c_sound_entity.rs
+++ azalea-protocol/src/packets/game/c_sound_entity.rs
@@ -1,27 +1,16 @@
 use azalea_buf::AzBuf;
 use azalea_protocol_macros::ClientboundGamePacket;
+use azalea_registry::SoundEvent;
+
+use super::c_sound::{CustomSound, SoundSource};
 
 #[derive(Clone, Debug, AzBuf, ClientboundGamePacket)]
 pub struct ClientboundSoundEntity {
+    pub sound: azalea_registry::Holder<SoundEvent, CustomSound>,
     pub source: SoundSource,
     #[var]
     pub id: u32,
     pub volume: f32,
     pub pitch: f32,
-    #[var]
     pub seed: u64,
-}
-
-#[derive(AzBuf, Clone, Copy, Debug)]
-pub enum SoundSource {
-    Master = 0,
-    Music = 1,
-    Records = 2,
-    Weather = 3,
-    Blocks = 4,
-    Hostile = 5,
-    Neutral = 6,
-    Players = 7,
-    Ambient = 8,
-    Voice = 9,
 }
