ad41d45ba0dc048576e3463aaef9d0cdc316e2f9
mat <git@matdoes.dev>
2025-02-03 19:28:25 +0000

    remove unnecessary clones

--- azalea-client/src/packet_handling/game.rs
+++ azalea-client/src/packet_handling/game.rs
@@ -289,10 +289,8 @@ pub fn process_packet_events(ecs: &mut World) {
                         let map = instance_holder.instance.read().registries.map.clone();
                         let new_registries = &mut weak_instance.write().registries;
                         // add the registries from this instance to the weak instance
-                        for (registry_name, registry) in &map {
-                            new_registries
-                                .map
-                                .insert(registry_name.clone(), registry.clone());
+                        for (registry_name, registry) in map {
+                            new_registries.map.insert(registry_name, registry);
                         }
                     }
                     instance_holder.instance = weak_instance;
