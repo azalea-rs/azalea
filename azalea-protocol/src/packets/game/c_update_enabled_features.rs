0fc0fe41d4a8194a0f10f8a13e8194e89eb05d06
mat <git@matdoes.dev>
2025-04-24 18:42:56 -0400

    delete some unused packet modules in game state

--- azalea-protocol/src/packets/game/c_update_enabled_features.rs
+++
@@ -1,8 +0,0 @@
-use azalea_buf::AzBuf;
-use azalea_core::resource_location::ResourceLocation;
-use azalea_protocol_macros::ClientboundGamePacket;
-
-#[derive(Clone, Debug, AzBuf, ClientboundGamePacket)]
-pub struct ClientboundUpdateEnabledFeatures {
-    pub features: Vec<ResourceLocation>,
-}
