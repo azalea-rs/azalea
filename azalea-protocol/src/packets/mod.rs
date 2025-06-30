ebc2e0c067d8b2c901ae02e032159e2c80eac7bc
mat <27899617+mat-1@users.noreply.github.com>
2025-06-30 14:14:45 -0500

    1.21.7 (#227)
    * 1.21.7-rc1
    
    * 1.21.7
    
    * update Cargo.lock too

--- azalea-protocol/src/packets/mod.rs
+++ azalea-protocol/src/packets/mod.rs
@@ -11,8 +11,8 @@ use azalea_buf::{AzaleaReadVar, AzaleaWrite, AzaleaWriteVar, BufReadError};
 
 use crate::read::ReadPacketError;
 
-pub const PROTOCOL_VERSION: i32 = 771;
-pub const VERSION_NAME: &str = "1.21.6";
+pub const PROTOCOL_VERSION: i32 = 772;
+pub const VERSION_NAME: &str = "1.21.7";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ConnectionProtocol {
