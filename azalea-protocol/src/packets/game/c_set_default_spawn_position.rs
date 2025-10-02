8def761654aeebf6b24a1b1f7c68bfbe4afff773
qwqawawow <eihqnh@outlook.com>
2025-10-02 13:50:46 +0800

    fix(protocol): update 1.21.9 ClientboundSetDefaultSpawnPosition fields

--- azalea-protocol/src/packets/game/c_set_default_spawn_position.rs
+++ azalea-protocol/src/packets/game/c_set_default_spawn_position.rs
@@ -1,9 +1,10 @@
 use azalea_buf::AzBuf;
-use azalea_core::position::BlockPos;
+use azalea_core::position::GlobalPos;
 use azalea_protocol_macros::ClientboundGamePacket;
 
 #[derive(Clone, Debug, AzBuf, PartialEq, ClientboundGamePacket)]
 pub struct ClientboundSetDefaultSpawnPosition {
-    pub pos: BlockPos,
-    pub angle: f32,
+    pub global_pos: GlobalPos,
+    pub yaw: f32,
+    pub pitch: f32,
 }
