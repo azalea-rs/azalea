feb845402c2c558e6d3a0820c44c5069c3889186
mat <git@matdoes.dev>
2025-12-09 10:52:02 -0630

    1.21.11

--- azalea-protocol/src/packets/mod.rs
+++ azalea-protocol/src/packets/mod.rs
@@ -11,8 +11,8 @@ use azalea_buf::{AzaleaReadVar, AzaleaWrite, AzaleaWriteVar, BufReadError};
 
 use crate::read::ReadPacketError;
 
-pub const PROTOCOL_VERSION: i32 = 1073742110;
-pub const VERSION_NAME: &str = "1.21.11-rc3";
+pub const PROTOCOL_VERSION: i32 = 774;
+pub const VERSION_NAME: &str = "1.21.11";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ConnectionProtocol {
