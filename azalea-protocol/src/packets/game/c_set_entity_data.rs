c35b57ebf82fa8b26ada77ab9eb795e3827d6c16
mat <git@matdoes.dev>
2026-07-13 07:08:42 +0300

    fix incorrect particle order for 26.2

--- azalea-protocol/src/packets/game/c_set_entity_data.rs
+++ azalea-protocol/src/packets/game/c_set_entity_data.rs
@@ -9,3 +9,23 @@ pub struct ClientboundSetEntityData {
     pub id: MinecraftEntityId,
     pub packed_items: EntityMetadataItems,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use azalea_buf::AzBuf;
+
+    use crate::packets::game::ClientboundSetEntityData;
+
+    #[test]
+    fn read_set_entity_data() {
+        // from hypixel
+        #[rustfmt::skip]
+        let contents = [173, 179, 148, 8, 10, 17, 1, 28, 38, 124, 175, 198, 255];
+        let mut buf = Cursor::new(contents.as_slice());
+        let _packet = ClientboundSetEntityData::azalea_read(&mut buf).unwrap();
+
+        assert_eq!(buf.position(), contents.len() as u64);
+    }
+}
